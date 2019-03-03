4be72670af0ff9d47fea22e7f3dfabd10c5f4722
Lexi <ambellinger24@gmail.com>
2019-03-02 22:37:52 -0600

    deployed

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var db = require("./models");
-var PORT = 3000;
 
-var PORT = process.env.PORT || 3000;   //When you go to connect your mongo //database to mongoose, do so the following way:
+
+var PORT = process.env.PORT || 3000;
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 
 //CODE THAT PHIL SENT US///
 // // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
@@ -38,8 +43,8 @@ var routes = require("./controllers/routes");
 
  //var routes = express.Router();
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoheadlinenews", { useNewUrlParser: true });
+// // Connect to the Mongo DB
+// mongoose.connect("mongodb://localhost/mongoheadlinenews", { useNewUrlParser: true });
 
 // Start the server
 app.listen(PORT, function() {
